a480f9e1b3b9c1792ba26f2c3c4e2ad946cba1fc
Aditya Kumar Dwivedi <129512051+akdwivedi7355@users.noreply.github.com>
2024-09-05 18:44:14 +0530

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# Check if Python is installed
+if ! command -v python3 &> /dev/null
+then
+    echo "Python is not installed. Please install Python."
+    exit
+fi
+
+# Check if pip is installed
+if ! command -v pip &> /dev/null
+then
+    echo "pip is not installed. Installing pip..."
+    python3 -m ensurepip --upgrade
+fi
+
 echo "Installing dlib from wheel..."
 pip install vendor/dlib-19.24.1-cp312-cp312-manylinux_2_17_x86_64.whl
 
-# Install Python dependencies
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
-
-# Install the manually downloaded dlib package
